9b0690d3e9205e6c52ffb41b5fb1e4f2a9f4d466
amirshokraie <amirshokraie@outlook.com>
2025-09-02 23:45:11 +0330

    added custom user model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User model using email as the unique identifier
+    instead of default username.
+    """ 
+    email = models.EmailField(unique=True, db_index=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+
+
+    email_confirmed = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
+    date_joined = models.DateTimeField(default=timezone.now)
+
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+
+
+    def __str__(self) -> str:
+        return self.email
+     
