5f0787d64a9424aaaf03d6a6d640ad6834bfe142
amirshokraie <amirshokraie@outlook.com>
2025-09-03 22:58:05 +0330

    added makemigrations

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput 2>/dev/null || true
 python manage.py runserver 0.0.0.0:8000
