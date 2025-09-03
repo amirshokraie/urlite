b6fb99af841aef7decf6b1498e184f6688a48611
amirshokraie <amirshokraie@outlook.com>
2025-09-03 08:45:48 +0330

    added UniqueValidator for email field of UserRegisterSerializer

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from .models import User
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
     password = serializers.CharField(write_only=True, min_length=8)
 
 
