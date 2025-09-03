a166609a4f22528e7c79239964ba5a833cea3d4e
amirshokraie <amirshokraie@outlook.com>
2025-09-03 16:19:10 +0330

    initialized RedirectAPIView

--- src/links/views.py
+++ src/links/views.py
@@ -1,8 +1,15 @@
+from django.shortcuts import redirect
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import LinkCreateSerializer
+from .models import Link
 
 
 class LinkCreateAPIView( CreateAPIView):
     serializer_class = LinkCreateSerializer
+
+class RedirectAPIView(APIView):
+    def get(self, request, code, **kw):
+        pass
