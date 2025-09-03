996007e56eb7d41481883be28cd799221ddec647
amirshokraie <amirshokraie@outlook.com>
2025-09-03 08:52:30 +0330

    added api views for registration, retrieving user data and changing password

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView, 
+    UpdateAPIView, 
+    RetrieveAPIView,
+)
+from rest_framework.permissions import IsAuthenticated
+from .serializers import (
+    UserRegisterSerializer,
+    UserRetrieveSerializer,
+    PasswordChangeSerializer
+)
+from .models import User
 
-# Create your views here.
+
+class UserRegisterAPIView(CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated,]
+    serializer_class = UserRetrieveSerializer
+    queryset = User.objects.all()
+
+    def get_object(self):
+        return self.request.user
+
+
+class PasswordChangeAPIView(UpdateAPIView):
+    permission_classes = [IsAuthenticated,]
+    serializer_class = PasswordChangeSerializer
