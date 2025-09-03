292c14961aba11722a940990d8d920d7bf757a62
amirshokraie <amirshokraie@outlook.com>
2025-09-03 15:06:01 +0330

    added LinkCreateAPIView

--- src/links/views.py
+++ src/links/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LinkCreateSerializer
 
-# Create your views here.
+
+class LinkCreateAPIView( CreateAPIView):
+    serializer_class = LinkCreateSerializer
