c27feeaf4b21eeb5cb648f095312d84a385ce8c1
amirshokraie <amirshokraie@outlook.com>
2025-09-03 16:21:26 +0330

    included links urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     # Accounts (JWT & user endpoints)
-    path('api/accounts/', include('accounts.urls'))
+    path('api/accounts/', include('accounts.urls')),
+    path('api/links/', include('links.urls'))
 ]
