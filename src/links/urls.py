0775d6b685a45caefb443a4feab3009ff006a0a0
amirshokraie <amirshokraie@outlook.com>
2025-09-03 17:15:50 +0330

    added UserLinkListAPIView's url

--- src/links/urls.py
+++ src/links/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import LinkCreateAPIView, RedirectView
+from .views import (
+    LinkCreateAPIView, 
+    RedirectView,
+    UserLinkListAPIView
+    )
 
 app_name = "links"
 
 urlpatterns = [
     path('create/', LinkCreateAPIView.as_view(), name='create_link'),
     path('r/<str:code>/', RedirectView.as_view(), name='redirect'),
+    path('list/', UserLinkListAPIView.as_view(), name='list_links'),
 ]
