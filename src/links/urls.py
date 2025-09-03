4763f73c20916965fae34bcd4a0dc6bcf94e4731
amirshokraie <amirshokraie@outlook.com>
2025-09-03 16:37:07 +0330

    fixed view name

--- src/links/urls.py
+++ src/links/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
-from .views import LinkCreateAPIView, RedirectAPIView
+from .views import LinkCreateAPIView, RedirectView
 
 app_name = "links"
 
 urlpatterns = [
     path('create/', LinkCreateAPIView.as_view(), name='create_link'),
-    path('r/<str:code>/', RedirectAPIView.as_view(), name='redirect'),
+    path('r/<str:code>/', RedirectView.as_view(), name='redirect'),
 ]
